9be32aa41efa2f148f755ec1cd68cff4b2afcc3f
Shimazaky80 <64324142+Shimazaky80@users.noreply.github.com>
2023-09-13 22:09:35 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Math.random() will return a random decimal
+// number between 0 and 1
+let x = Math.random()
+
+// if wanting to get a random number between
+// 1 and 6
+// will return a random number
+// between 1 and 5
+x = Math.random() * 6
+console.log(x)
